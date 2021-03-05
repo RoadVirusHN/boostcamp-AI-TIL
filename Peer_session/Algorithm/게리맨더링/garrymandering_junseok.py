8b29874ff82388ef55ca807f1eb56d3f5de0f7d3
YUNJUNSEOK <roadvirushn@gmail.com>
2021-03-05 11:11:00 +0900

    algorithm done

--- Peer_session/Algorithm/게리맨더링/garrymandering_junseok.py
+++ Peer_session/Algorithm/게리맨더링/garrymandering_junseok.py
@@ -1,9 +1,38 @@
 import sys
 import itertools
+
+def isSCC(vertices):
+    global graph
+    visited = [False for _ in range(len(vertices))]
+    q = [vertices[0]]
+    visited[0] = True
+    while q:
+        now = q.pop(0)
+        for neighbor in graph[now]:
+            if neighbor in vertices and not visited[vertices.index(neighbor)]:
+                visited[vertices.index(neighbor)] = True
+                q.append(neighbor)
+    if False in visited:
+        return False
+    else:
+        return True
+
 input = sys.stdin.readline
 areaNum = int(input())
 pops = list(map(int, input().split()))
+allPops = sum(pops)
+minimum = allPops + 1
 graph = [list(map(lambda x: x-1,list(map(int, input().split()))[1:])) for _ in range(areaNum)]
-
-
-print(list(list(itertools.combinations([i for i in range(areaNum)],j)) for j in range(areaNum//2, 0, -1)))
+areas = list(itertools.chain.from_iterable((itertools.combinations([i for i in range(areaNum)],j)) for j in range(areaNum//2, 0, -1)))
+# itertools.chain.from_iterable(): squeeze
+visited = []
+for group in areas:
+    group = list(group)
+    groupPop = sum([pops[popidx] for popidx in group])
+    if abs(allPops - 2*groupPop) < minimum:
+        if isSCC(group) and isSCC([i for i in range(areaNum) if i not in group]):
+            minimum = abs(allPops - 2*groupPop)
+if minimum == allPops + 1:
+    print(-1)
+else:
+    print(minimum)
--- Peer_session/Algorithm/스타트링크 타워/startlink_tower_junseok.py
+++ Peer_session/Algorithm/스타트링크 타워/startlink_tower_junseok.py
@@ -1,2 +1,46 @@
 import sys
+sys.setrecursionlimit(987654321)
 input = sys.stdin.readline
+
+nums = '''###...#.###.###.#.#.###.###.###.###.###
+#.#...#...#...#.#.#.#...#.....#.#.#.#.#
+#.#...#.###.###.###.###.###...#.###.###
+#.#...#.#.....#...#...#.#.#...#.#.#...#
+###...#.###.###...#.###.###...#.###.###'''.split('\n')
+
+nums = [[nums[j][i*4:i*4+3] for j in range(5)] for i in range(10)]
+noSharps = [[[] for __ in range(3)] for _ in range(5)]
+for i in range(5):
+    for j in range(3):
+        for numidx in range(10):
+            if nums[numidx][i][j] == '.':
+                noSharps[i][j].append(numidx)
+
+
+numCount = int(input())
+display = [input().rstrip('\n') for _ in range(5)]
+display = [[display[j][i*4:i*4+3] for j in range(5)] for i in range(numCount)]
+
+cases = [[] for _ in range(numCount)]
+
+for idx in range(numCount):
+    available =[i for i in range(10)]
+    for i in range(5):
+        for j in range(3):
+            if display[idx][i][j] == '#':
+                for unavail in noSharps[i][j]:
+                    if unavail in available : available.remove(unavail)
+    cases[idx] = [i for i in available]
+
+answer = 0
+caseNum = 1
+for i in range(numCount):
+    caseNum *= len(cases[i])
+if caseNum:
+    multiply = 1
+    for caseIdx in range(len(cases)-1, -1, -1):
+        answer += sum(cases[caseIdx])*(caseNum/len(cases[caseIdx])) * multiply
+        multiply *= 10    
+    print(answer/caseNum)
+else:
+    print(-1)
